16ae2039d04a44ce7c178f2d935f42cfac22cd3b
ecliujianbo <ecliujianbo@126.com>
2016-02-26 18:13:49 +0800

    add sample.

--- app/src/main/java/com/liu/lframelib/net/BitmapLruCache.java
+++ app/src/main/java/com/liu/lframelib/net/BitmapLruCache.java
@@ -1,36 +1,34 @@
 package com.liu.lframelib.net;
-//
-//package com.otouzi.p2p.net;
-//
-//import android.graphics.Bitmap;
-//import android.support.v4.util.LruCache;
-//
-//import com.android.volley.toolbox.ImageLoader;
-//
-///**
-// * 缓存
-// * 
-// * 
-// *         2014-9-2
-// */
-//public class BitmapLruCache extends LruCache<String, Bitmap> implements
-//		ImageLoader.ImageCache {
-//	public BitmapLruCache(int maxSize) {
-//		super(maxSize);
-//	}
-//
-//	@Override
-//	protected int sizeOf(String key, Bitmap bitmap) {
-//		return bitmap.getRowBytes() * bitmap.getHeight();
-//	}
-//
-//	@Override
-//	public Bitmap getBitmap(String url) {
-//		return get(url);
-//	}
-//
-//	@Override
-//	public void putBitmap(String url, Bitmap bitmap) {
-//		put(url, bitmap);
-//	}
-//}
+
+import android.graphics.Bitmap;
+import android.support.v4.util.LruCache;
+
+import com.android.volley.toolbox.ImageLoader;
+
+/**
+ * 缓存
+ * <p>
+ * <p>
+ * 2014-9-2
+ */
+public class BitmapLruCache extends LruCache<String, Bitmap> implements
+        ImageLoader.ImageCache {
+    public BitmapLruCache(int maxSize) {
+        super(maxSize);
+    }
+
+    @Override
+    protected int sizeOf(String key, Bitmap bitmap) {
+        return bitmap.getRowBytes() * bitmap.getHeight();
+    }
+
+    @Override
+    public Bitmap getBitmap(String url) {
+        return get(url);
+    }
+
+    @Override
+    public void putBitmap(String url, Bitmap bitmap) {
+        put(url, bitmap);
+    }
+}
--- app/src/main/java/com/liu/lframelib/ui/load/BaseLoadingActivity.java
+++ app/src/main/java/com/liu/lframelib/ui/load/BaseLoadingActivity.java
@@ -1,155 +1,208 @@
 package com.liu.lframelib.ui.load;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Intent;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Toast;
 
-import java.lang.reflect.ParameterizedType;
-
 import com.liu.lframelib.BaseActivity;
 import com.liu.lframelib.R;
 import com.liu.lframelib.utils.JsonTools;
+import com.liu.lframelib.utils.ToastUtils;
 import com.liu.lframelib.widget.PreLoadingView;
 
+import java.lang.reflect.ParameterizedType;
+
 /**
+ * <p>
+ * use this ni need use <b>R.id.loading_root_view</b> as rootView,preLoadingView need attach.
+ * <b>R.id.loading_content_view</b> as the content view.
+ * </p>
+ * <p/>
+ * <p/>
  * Created by liu on 2016-2-22.
  */
 public abstract class BaseLoadingActivity<T> extends BaseActivity {
-	private View mLoadingView;
-	private PreLoadingView mDefaultLoadingView;
-	private View mRootLoadingView;
-	private Class<T> mClazz;
-	/**
-	 * 是否有缓存
-	 */
-	private boolean mCacheAvailable = false;
+    private View mLoadingView;
+    private View mContentView;
+    private PreLoadingView mDefaultLoadingView;
+    private View mRootView;
+    private Class<T> mClazz;
+    /**
+     * 是否有缓存
+     */
+    private boolean mCacheAvailable = false;
 
-	@Override
-	protected void initData() {
-		initLoadingView();
-		String cache = getCacheData();
-		// no cache
-		if (TextUtils.isEmpty(cache)) {
-			mCacheAvailable = false;
-			if (isNetworkAvailable()) {
-				initNetData();
-			} else {
-				// show loading failure
-				if (mLoadingView != null && mLoadingView instanceof ILoadingView) {
-					((ILoadingView) mLoadingView).showLoadFailedStatus();
-				}
-			}
-		} else {
-			mCacheAvailable = true;
-			// hidden loading,show cache
-			if (mLoadingView != null && mLoadingView instanceof ILoadingView) {
-				((ILoadingView) mLoadingView).hiddenLoadingView();
-				initCacheData(parseJson(cache));
-			}
-			if (isNetworkAvailable()) {
-				initNetData();
-			}
-		}
-	}
+    private int mShortAnimationDuration;
 
-	public void setCacheAvailable(boolean available) {
-		this.mCacheAvailable = available;
-	}
+    @Override
+    protected void initData() {
+        initLoadingView();
+        String cache = getCacheData();
+        // no cache
+        if (TextUtils.isEmpty(cache)) {
+            mCacheAvailable = false;
+            if (isNetworkAvailable()) {
+                initNetData();
+            } else {
+                // show loading failure
+                if (mLoadingView != null && mLoadingView instanceof ILoadingView) {
+                    ((ILoadingView) mLoadingView).showLoadFailedStatus();
+                }
+            }
+        } else {
+            mCacheAvailable = true;
+            // hidden loading,show cache
+            if (mLoadingView != null && mLoadingView instanceof ILoadingView) {
+                ((ILoadingView) mLoadingView).hiddenLoadingView();
+                initCacheData(parseJson(cache));
+            }
+            if (isNetworkAvailable()) {
+                initNetData();
+            }
+        }
+    }
 
-	public boolean isCacheAvailable() {
-		return mCacheAvailable;
-	}
+    public void setCacheAvailable(boolean available) {
+        this.mCacheAvailable = available;
+    }
 
-	protected T parseJson(String jsonStr) {
-		if (TextUtils.isEmpty(jsonStr)) {
-			return null;
-		}
-		ParameterizedType genericSuperclass = (ParameterizedType) getClass().getGenericSuperclass();
-		mClazz = (Class<T>) genericSuperclass.getActualTypeArguments()[0];
-		T t = JsonTools.jsonObj(jsonStr, mClazz);
-		if (t == null) {
-			return null;
-		}
-		return t;
-	}
+    public boolean isCacheAvailable() {
+        return mCacheAvailable;
+    }
 
-	private void initLoadingView() {
-		if (getLoadingView() == null) {
-			mDefaultLoadingView = new PreLoadingView(this);
-			mLoadingView = mDefaultLoadingView;
-		} else {
-			mLoadingView = getLoadingView();
-		}
-		if (mLoadingView instanceof ILoadingView) {
-			mRootLoadingView = findViewById(R.id.loading_root_view);
-			((ILoadingView) mLoadingView).setTargetView(mRootLoadingView);
-			((ILoadingView) mLoadingView).setOnRetryClickListener(new PreLoadingView.OnRetryClickListener() {
-				@Override
-				public void onRetryClickListener(View v) {
-					if (isNetworkAvailable()) {
-						initNetData();
-					} else {
-						Toast.makeText(getApplicationContext(), "网络不给力", Toast.LENGTH_SHORT).show();
-					}
-				}
-			});
-		}
+    protected T parseJson(String jsonStr) {
+        if (TextUtils.isEmpty(jsonStr)) {
+            return null;
+        }
+        ParameterizedType genericSuperclass = (ParameterizedType) getClass().getGenericSuperclass();
+        mClazz = (Class<T>) genericSuperclass.getActualTypeArguments()[0];
+        T t = JsonTools.jsonObj(jsonStr, mClazz);
+        if (t == null) {
+            return null;
+        }
+        return t;
+    }
 
-	}
+    private void initLoadingView() {
+        mContentView = findViewById(R.id.loading_content_view);
+        // Retrieve and cache the system's default "short" animation time.
+        mShortAnimationDuration = getResources().getInteger(android.R.integer.config_shortAnimTime);
+        if (getLoadingView() == null) {
+            mDefaultLoadingView = new PreLoadingView(this);
+            mLoadingView = mDefaultLoadingView;
+        } else {
+            mLoadingView = getLoadingView();
+        }
+        if (mLoadingView instanceof ILoadingView) {
+            mRootView = findViewById(R.id.loading_root_view);
+            ((ILoadingView) mLoadingView).setTargetView(mRootView);
+            ((ILoadingView) mLoadingView).setOnRetryClickListener(new PreLoadingView.OnRetryClickListener() {
+                @Override
+                public void onRetryClickListener(View v) {
+                    if (isNetworkAvailable()) {
+                        initNetData();
+                    } else {
+                        ToastUtils.getInstance(getApplicationContext(), R.string.net_work_error, Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
 
-	protected boolean isNeedLoadingView() {
-		return true;
-	}
+    }
 
-	protected void showLoadingStatus() {
-		if (mLoadingView != null && mLoadingView instanceof ILoadingView) {
-			((ILoadingView) mLoadingView).showLoadingStatus();
-		}
-	}
+    protected boolean isNeedLoadingView() {
+        return true;
+    }
 
-	protected void showLoadingFailStatus() {
-		if (mLoadingView != null && mLoadingView instanceof ILoadingView) {
-			((ILoadingView) mLoadingView).showLoadFailedStatus();
-		}
-	}
+    protected void showLoadingStatus() {
+        showContentOrLoadingIndicator(false);
+        ((ILoadingView) mLoadingView).showLoadingStatus();
+    }
 
-	protected void hiddenLoadingView() {
-		if (mLoadingView != null && mLoadingView instanceof ILoadingView) {
-			((ILoadingView) mLoadingView).hiddenLoadingView();
-		}
-	}
+    protected void showLoadingFailStatus() {
+//		if (mLoadingView != null && mLoadingView instanceof ILoadingView) {
+        showContentOrLoadingIndicator(false);
+        ((ILoadingView) mLoadingView).showLoadFailedStatus();
+//		}
+    }
 
-	/**
-	 * loadingView,must impl ILoadingView
-	 *
-	 * @return
-	 */
-	protected View getLoadingView() {
-		return null;
-	}
+    protected void hiddenLoadingView() {
+//		if (mLoadingView != null && mLoadingView instanceof ILoadingView) {
+//			((ILoadingView) mLoadingView).hiddenLoadingView();
+//		}
+        showContentOrLoadingIndicator(true);
+    }
 
-	/**
-	 * cache data
-	 *
-	 * @return
-	 */
-	protected abstract String getCacheData();
+    /**
+     * Cross-fades between {@link #mContentView} and {@link #mLoadingView}.
+     */
+    private void showContentOrLoadingIndicator(boolean contentLoaded) {
+        // Decide which view to hide and which to show.
+        final View showView = contentLoaded ? mContentView : mLoadingView;
+        final View hideView = contentLoaded ? mLoadingView : mContentView;
 
-	protected abstract void initCacheData(T t);
+        // Set the "show" view to 0% opacity but visible, so that it is visible
+        // (but fully transparent) during the animation.
+        showView.setAlpha(0f);
+        showView.setVisibility(View.VISIBLE);
 
-	protected abstract void initNetData();
+        // Animate the "show" view to 100% opacity, and clear any animation listener set on
+        // the view. Remember that listeners are not limited to the specific animation
+        // describes in the chained method calls. Listeners are set on the
+        // ViewPropertyAnimator object for the view, which persists across several
+        // animations.
+        showView.animate()
+                .alpha(1f)
+                .setDuration(mShortAnimationDuration)
+                .setListener(null);
 
-	@Override
-	protected void beforeContentView() {
-	}
+        // Animate the "hide" view to 0% opacity. After the animation ends, set its visibility
+        // to GONE as an optimization step (it won't participate in layout passes, etc.)
+        hideView.animate()
+                .alpha(0f)
+                .setDuration(mShortAnimationDuration)
+                .setListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        hideView.setVisibility(View.GONE);
+                    }
+                });
+    }
 
-	@Override
-	protected void obtainIntent(Intent intent) {
-	}
+    /**
+     * loadingView,must impl ILoadingView
+     *
+     * @return
+     */
+    protected View getLoadingView() {
+        return null;
+    }
 
-	@Override
-	protected void initListener() {
 
-	}
+    /**
+     * cache data
+     *
+     * @return
+     */
+    protected abstract String getCacheData();
+
+    protected abstract void initCacheData(T t);
+
+    protected abstract void initNetData();
+
+    @Override
+    protected void beforeContentView() {
+    }
+
+    @Override
+    protected void obtainIntent(Intent intent) {
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
 }
